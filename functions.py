5fabe30a7b335ffd86ad1056de4b4e9ab0d150eb
SloTGreaD <bayonetttt74@gmail.com>
2024-03-03 00:30:53 +0800

    first commit

---
+++ functions.py
@@ -0,0 +1,263 @@
+import random
+import sqlite3
+import time
+
+from datetime import datetime, timedelta
+misha_bot_api = '5629818025:AAE3CAZFs6uhMcWZodFUdpKhSJu5awmGK_o'
+poke_bot_api = "6831587612:AAEUQ4m30-Pajetdnw0AwZ4omaNmzVkc-4o"
+
+pokemon_list = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'NidoranF', 'Nidorina', 'Nidoqueen', 'NidoranM', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetchd', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr_Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew']
+rarity = {
+    "E": ['Bellsprout', 'Caterpie', 'Diglett', 'Ekans', 'Exeggcute', 'Gastly', 'Goldeen', 'Horsea', 'Krabby', 'Magikarp', 'NidoranF', 'NidoranM', 'Oddish', 'Omanyte', 'Paras', 'Pidgey', 'Poliwag', 'Rattata', 'Shellder', 'Spearow', 'Weedle', 'Zubat'],
+    "D": ['Abra', 'Clefairy', 'Dewgong', 'Doduo', 'Drowzee', 'Dugtrio', 'Geodude', 'Grimer', 'Growlithe', 'Kakuna', 'Koffing', 'Machop', 'Magnemite', 'Mankey', 'Meowth', 'Metapod', 'Pidgeotto', 'Psyduck', 'Sandshrew', 'Seel', 'Staryu', 'Tentacool', 'Venonat', 'Voltorb', 'Weepinbell'],
+    "C": ['Beedrill', 'Bulbasaur', 'Chansey', 'Charmander', 'Cubone', 'Eevee', 'Electrode', 'Fearow', 'Gloom', 'Golbat', 'Graveler', 'Haunter', 'Jigglypuff', 'Jynx', 'Kadabra', 'Kingler', 'Nidorina', 'Nidorino', 'Omastar', 'Parasect', 'Persian', 'Pikachu', 'Pinsir', 'Poliwhirl', 'Ponyta', 'Raticate', 'Rhyhorn', 'Seaking', 'Slowpoke', 'Squirtle', 'Tangela', 'Venomoth', 'Vulpix'],
+    "B": ['Arbok', 'Butterfree', 'Charmeleon', 'Clefable', 'Cloyster', 'Dodrio', 'Dratini', 'Electabuzz', 'Exeggutor', 'Golduck', 'Hitmonchan', 'Hitmonlee', 'Hypno', 'Ivysaur', 'Kabuto', 'Kangaskhan', 'Lapras', 'Lickitung', 'Machoke', 'Magmar', 'Magneton', 'Mr_Mime', 'Onix', 'Pidgeot', 'Poliwrath', 'Porygon', 'Primeape', 'Rapidash', 'Sandslash', 'Scyther', 'Seadra', 'Slowbro', 'Starmie', 'Tauros', 'Tentacruel', 'Victreebel', 'Vileplume', 'Wartortle', 'Weezing', 'Wigglytuff'],
+    "A": ['Aerodactyl', 'Alakazam', 'Arcanine', 'Blastoise', 'Charizard', 'Dragonair', 'Farfetchd', 'Flareon', 'Gengar', 'Golem', 'Gyarados', 'Jolteon', 'Kabutops', 'Machamp', 'Marowak', 'Muk', 'Nidoking', 'Nidoqueen', 'Ninetales', 'Raichu', 'Rhydon', 'Snorlax', 'Vaporeon', 'Venusaur'],
+    "S": ['Zapdos', 'Moltres', 'Mewtwo', 'Mew', 'Dragonite', 'Ditto', 'Articuno']
+}
+
+generations = {'Bellsprout': '', 'Caterpie': '', 'Diglett': '', 'Ekans': '', 'Exeggcute': '', 'Gastly': '', 'Goldeen': '', 'Horsea': '', 'Krabby': '', 'Magikarp': '', 'NidoranF': '', 'NidoranM': '', 'Oddish': '', 'Omanyte': '', 'Paras': '', 'Pidgey': '', 'Poliwag': '', 'Rattata': '', 'Shellder': '', 'Spearow': '', 'Weedle': '', 'Zubat': '', 'Abra': '', 'Clefairy': '', 'Dewgong': "Seel's evolution", 'Doduo': '', 'Drowzee': '', 'Dugtrio': "Diglett's evolution", 'Geodude': '', 'Grimer': '', 'Growlithe': '', 'Kakuna': "Weedle's 1'st evolution", 'Koffing': '', 'Machop': '', 'Magnemite': '', 'Mankey': '', 'Meowth': '', 'Metapod': "Caterpie's 1'st evolution", 'Pidgeotto': "Pidgey's 1'st evolution", 'Psyduck': '', 'Sandshrew': '', 'Seel': '', 'Staryu': '', 'Tentacool': '', 'Venonat': '', 'Voltorb': '', 'Weepinbell': "Bellsprout's 1'st evolution", 'Beedrill': "Weedle's 2'nd evolution", 'Bulbasaur': '', 'Chansey': '', 'Charmander': '', 'Cubone': '', 'Eevee': '', 'Electrode': "Voltorb's evolution", 'Fearow': "Spearow's evolution", 'Gloom': "Oddish's 1'st evolution", 'Golbat': "Zubat's evolution", 'Graveler': "Geodude's 1'st evolution", 'Haunter': "Gastly's 1'st evolution", 'Jigglypuff': '', 'Jynx': '', 'Kadabra': "Abra's 1'st evolution", 'Kingler': "Krabby's evolution", 'Nidorina': "NidoranF's 1'st evolution", 'Nidorino': "NidoranM's evolution", 'Omastar': "Omanyte's evolution", 'Parasect': "Paras's evolution", 'Persian': "Meowth's evolution", 'Pikachu': '', 'Pinsir': '', 'Poliwhirl': "Poliwag's 1'st evolution", 'Ponyta': '', 'Raticate': "Rattata's evolution", 'Rhyhorn': '', 'Seaking': "Goldeen's evolution", 'Slowpoke': '', 'Squirtle': '', 'Tangela': '', 'Venomoth': "Venonat's evolution", 'Vulpix': '', 'Arbok': "Ekan's evolution", 'Butterfree': "Caterpie's 2'nd evolution", 'Charmeleon': "Charmander's 1'st evolution", 'Clefable': "Clefairy's evolution", 'Cloyster': "Shellder's evolution", 'Dodrio': "Doduo's evolution", 'Dratini': '', 'Electabuzz': '', 'Exeggutor': "Exeggcute's evolution", 'Golduck': "Psyduck's evolution", 'Hitmonchan': '', 'Hitmonlee': '', 'Hypno': "Drowzee's evolution", 'Ivysaur': "Bulbasaur's 1'st evolution", 'Kabuto': '', 'Kangaskhan': '', 'Lapras': '', 'Lickitung': '', 'Machoke': "Machop's 1'st evolution", 'Magmar': '', 'Magneton': "Magnemite's evolution", 'Mr_Mime': '', 'Onix': '', 'Pidgeot': "Pidgey's 2'st evolution", 'Poliwrath': "Poliwag's 2'nd evolution", 'Porygon': '', 'Primeape': "Mankey's evolution", 'Rapidash': "Ponyta's evolution", 'Sandslash': "Sandshrew's evolution", 'Scyther': '', 'Seadra': "Horsea's evolution", 'Slowbro': "Slowpoke's evolution", 'Starmie': "Staryu's evolution", 'Tauros': '', 'Tentacruel': "Tentacool's evolution", 'Victreebel': "Bellsprout's 2'nd evolution", 'Vileplume': "Oddish's 2'nd evolution", 'Wartortle': "Squirtle's 1'st evolution", 'Weezing': "Koffing's evolution", 'Wigglytuff': "Jigglypuff's evolution", 'Aerodactyl': '', 'Alakazam': "Abra's 2'nd evolution", 'Arcanine': "Growlithe's evolution", 'Blastoise': "Squirtle's 2'nd evolution", 'Charizard': "Charmander's 2'nd evolution", 'Dragonair': "Dratini's 1'st evolution", 'Farfetchd': '', 'Flareon': "Eevee's evolution", 'Gengar': "Gastly's 2'nd evolution", 'Golem': "Geodude's 2'nd evolution", 'Gyarados': "Magikarp's evolution", 'Jolteon': "Eevee's evolution", 'Kabutops': "Kabuto's evolution", 'Machamp': "Machop's 2'nd evolution", 'Marowak': "Cubone's evolution", 'Muk': "Grimer's evolution", 'Nidoking': "NidoranM's evolution", 'Nidoqueen': "Nidorina's evolution", 'Ninetales': "Vulpix's evolution", 'Raichu': "Pikachu's evolution", 'Rhydon': "Rhyhorn's evolution", 'Snorlax': '', 'Vaporeon': "Eevee's evolution", 'Venusaur': "Bulbasaur's 2'nd evolution", 'Zapdos': '', 'Moltres': '', 'Mewtwo': '', 'Mew': '', 'Dragonite': "Dratini's 2'nd evolution", 'Ditto': '', 'Articuno': ''}
+
+def pokemon_catch():  # возвращает рандомное имя покемона в зависимости от их вероятности выпадения
+    dictio = {"E":'600',
+              "D":'230',
+              "C":'120',       # key - веротность выпаения покемона, value -  list с именами покемонов
+              "B":'30',
+              "A":'19',   # вероятности должны быть написаны целыми числами
+              "S":'1'}
+
+
+
+    rand_num = random.randint(1, 1000) # вероятности должны в сумме давать 1000
+    counter = 0
+    for key in dictio:
+        counter += int(dictio[key])
+        if counter >= rand_num:
+            pokemon_name = random.choice(rarity[key])
+            return pokemon_name, key
+        
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn.cursor()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.commit()
+        self.conn.close()
+
+
+def create_users_table():
+    conn = sqlite3.connect('pokedex.sql')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS users (name varchar(50))')
+    conn.commit()
+    conn.close()
+
+
+
+def create_captured_pokemons_table():
+    conn = sqlite3.connect('pokedex.sql')
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS captured_pokemons (
+        user_id INTEGER,
+        found_pokemon VARCHAR(20),
+        captured_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        ''')
+    conn.commit()
+    conn.close()
+
+def create_number_of_pokemons(): #таблица для учета количества покемонов у каждого юзера
+    conn = sqlite3.connect('pokedex.sql')
+    cur = conn.cursor()
+
+    text = "CREATE TABLE IF NOT EXISTS number_of_pokemons (user_id INTEGER, last_access_date VARCHAR(12) DEFAULT '10/12/15', pokebols INTEGER DEFAULT 5, "
+    # for item in pokemon_list:     #создаем таблицу с покемонами из этого листа, который был идентифицирован ранее
+    #     text += f'{item.lower()} INTEGER DEFAULT 0,'
+    text += "".join(f'{item.lower()} INTEGER DEFAULT 0,' for item in pokemon_list)
+    text = text.rstrip(',') + ")"
+    cur.execute(text)
+    conn.commit()
+    conn.close()
+
+def create_all_tables():
+    with DatabaseConnection('pokedex.sql') as cur:
+        text = "CREATE TABLE IF NOT EXISTS number_of_pokemons (user_id INTEGER, last_access_date VARCHAR(12) DEFAULT '10/12/15', pokebols INTEGER DEFAULT 5,  "
+        # for item in pokemon_list:     #создаем таблицу с покемонами из этого листа, который был идентифицирован ранее
+        #     text += f'{item.lower()} INTEGER DEFAULT 0,'
+        text += "".join(f'{item.lower()} INTEGER DEFAULT 0,' for item in pokemon_list)
+        text = text.rstrip(',') + ")"
+        cur.execute(text)
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS captured_pokemons (
+            user_id INTEGER,
+            found_pokemon VARCHAR(20),
+            captured_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            ''')
+        cur.execute('CREATE TABLE IF NOT EXISTS users (name varchar(50))')
+    
+
+def add_user_to_number_of_pokemons(user_id):
+    with DatabaseConnection('pokedex.sql') as cur:
+        check_query = 'SELECT * FROM number_of_pokemons WHERE user_id = ?'
+        cur.execute(check_query, (user_id,))
+        result = cur.fetchone()  # Получаем результат выполнения запроса
+        if result is None:  # Проверяем, существует ли уже пользователь
+            insert_query = "INSERT INTO number_of_pokemons (user_id) VALUES (?)"
+            cur.execute(insert_query, (user_id,))
+
+    
+
+def capture_pokemon(user_id, found_pokemon): #добавляет название покемона в таблицу с временами когда словил покемона и таблицу с количеством словленных покемонов
+    with DatabaseConnection('pokedex.sql') as cur:
+        # Проверяем количество pokebols у пользователя
+        num = 'SELECT pokebols FROM number_of_pokemons WHERE user_id = ?'
+        cur.execute(num, (user_id,))
+        pokebol_count = cur.fetchone()[0]
+
+        if pokebol_count > 0:
+            found_pokemon = found_pokemon.lower()
+            # Выполняем логику захвата
+            cap = "INSERT INTO captured_pokemons (user_id, found_pokemon) VALUES (?, ?)"
+            cur.execute(cap, (user_id, found_pokemon))
+            query = f"UPDATE number_of_pokemons SET {found_pokemon} = {found_pokemon} + 1, pokebols = pokebols - 1 WHERE user_id = ?"
+            cur.execute(query, (user_id,))
+        
+            success = True
+        else:
+            success = False
+
+    
+    return success
+
+def capture_failed (user_id):
+    with DatabaseConnection('pokedex.sql') as cur:
+        pok = 'SELECT pokebols FROM number_of_pokemons WHERE user_id = ?'
+        cur.execute(pok, (user_id,))
+        pokebol_count = cur.fetchone()[0]
+
+        if pokebol_count > 0:
+            num2 = "UPDATE number_of_pokemons SET pokebols = pokebols - 1 WHERE user_id = ?"
+            cur.execute(num2, (user_id,))
+        
+
+def show_capture_time(user_id): #показывает время когда словил каждого покемона
+    with DatabaseConnection('pokedex.sql') as cur:
+        cap1 = 'SELECT * FROM captured_pokemons WHERE user_id = ?'
+        cur.execute(cap1, (user_id,))
+        info = cur.fetchall()
+        pokedex = ''
+        for el in info:
+            # Убедитесь, что здесь правильно форматируете строку, например:
+            pokedex += f"Pokemon: {el[1]}, Captured At: {el[2]}\n"
+    
+    return pokedex
+
+
+def show_pokedex(user_id):
+    with DatabaseConnection('pokedex.sql') as cur:
+        num3 = 'SELECT * FROM number_of_pokemons WHERE user_id = ?'
+        cur.execute(num3, (user_id,))
+        #собирает всю информацию о покемонах и конкатенирует все в лист_lines_list
+        pokemon_amount = cur.fetchone()[3:]
+        pokemons = ("".join(f'{pokemon} {"🟢" if amount>0 else "🔴"}') for pokemon, amount in zip(pokemon_list, pokemon_amount))
+        lines = (f"{num}. {pokemon}" for num, pokemon in enumerate(pokemons, 1))
+        lines_list = list(lines)
+        
+        while True:
+            #разбивает лист на равные куски по 25 покемонов, (последний кусок 26) и возвращает ганератор с нужным текстом
+            for chunk_start in range(0, 150, 25):
+                if chunk_start == 125:
+                    yield '\n'.join(lines_list[chunk_start : chunk_start + 26])
+                else:
+                    yield '\n'.join(lines_list[chunk_start : chunk_start + 25])
+
+
+def my_pokemons(user_id):
+    with DatabaseConnection('pokedex.sql') as cur:
+        num4 = 'SELECT * FROM number_of_pokemons WHERE user_id = ?'
+        cur.execute(num4, (user_id,))
+        pokemons = cur.fetchone()
+
+        if pokemons is None:
+            return "You haven't caught any Pokémon yet."
+        
+        text = f'You have:\nPokebols: {pokemons[2]}'
+        # for poke_count, pokemon_name in zip(pokemons[3:], pokemon_list):
+        #     if poke_count > 0:
+        #         text += f'{pokemon_name}: {poke_count}\n'
+        text = '\n'.join((text,"\n".join(f'{pokemon_name}: {poke_count}' for poke_count, pokemon_name in zip(pokemons[3:], pokemon_list) if poke_count > 0)))
+        #     text = f"""You have:
+        # Pokebols: {pokemons[2]}
+        # Pikachu: {pokemons[3]}
+        # Squirtle: {pokemons[4]}
+        # Bulbasaur: {pokemons[5]}
+        # Charmander: {pokemons[6]}
+        # """
+    
+    return text
+
+def add_pokebols(user_id, amount, cur):
+    with DatabaseConnection('pokedex.sql') as cur:
+        update_query = "UPDATE number_of_pokemons SET pokebols = pokebols + ? WHERE user_id = ?"
+        cur.execute(update_query, (amount, user_id))
+
+def pokebols_number(user_id):
+    number = 0
+    with DatabaseConnection('pokedex.sql') as cur:
+        num5 = 'SELECT pokebols FROM number_of_pokemons WHERE user_id = ?'
+        cur.execute(num5, (user_id,))
+        result = cur.fetchone()
+        if result:
+            number = int(result[0])
+    return number
+
+
+
+#проверяет последнюю дату запроса юзера на получение покеболов, и если это новый день, дает разрешение и перезаписывает дату
+def check_pokebols_elegibility(user_id):
+    can_get_pokemons = False
+    with DatabaseConnection('pokedex.sql') as cursor:
+        num6 = 'SELECT last_access_date from number_of_pokemons where user_id = ?'
+        cursor.execute(num6, (user_id,))
+        date = cursor.fetchone()[0]
+        now = datetime.now()
+        current_date = now.strftime("%d/%m/%y")
+        if date != current_date:
+            can_get_pokemons = True
+            num7 = "UPDATE number_of_pokemons SET last_access_date = ? WHERE user_id = ?"
+            cursor.execute(num7, (current_date, user_id))
+    return can_get_pokemons
+
+def time_until_next_midnight():
+    current_time = datetime.now()
+    next_midnight = datetime(current_time.year, current_time.month, current_time.day) + timedelta(days=1)
+    time_remaining = next_midnight - current_time
+    hours, remainder = divmod(time_remaining.seconds, 3600)
+    minutes = remainder // 60
+    return f'{hours} часов, {minutes+1} минут'
+
+
+
+
+if __name__ == "__main__":
+    # print(show_pokedex(668210174))
+    # print(time_until_next_midnight())
+    a = show_pokedex(668210174)
+    print(a.__next__())
+
+
+
+
+
+
+
+
+
---
+++ main1.py
@@ -0,0 +1,227 @@
+import telebot
+from telebot import types
+import random
+import functions
+import sqlite3
+from functions import DatabaseConnection
+from info import helpinfo
+from info import rarity
+# Загрузка токена из переменных окружения
+bot = telebot.TeleBot(functions.poke_bot_api)
+
+found_pokemon = []
+
+
+class PokemonBot:
+
+
+    def __init__(self):
+        # Словарь для хранения состояний пользователей
+        self.states = {}
+        self.generator = None
+        # Создаем таблицу для спойманных покемонов
+        functions.create_all_tables()
+
+    def start(self, message):
+
+        functions.add_user_to_number_of_pokemons(message.chat.id) #добавляет только новых юзеров
+        # Приветственное сообщение при старте
+        bot.send_message(message.chat.id, f"Hi, {message.from_user.first_name}!\nWelcome to Poké-Hunter. This bot allows you to search and catch Pokémons.\nPress /go to start your adventure.\nPress /help for more information.")
+
+
+    def handle_go_callback(self, call):
+        chat_id = call.message.chat.id # Для простоты предполагаем, что user_id и chat_id идентичны
+
+        pokebol_count = functions.pokebols_number(chat_id)
+
+        if pokebol_count > 0:
+            # Обработка нажатия кнопок "Go", "Keep going", "Skip"
+            if call.data in ['go', 'keepgoing', 'skip']:
+                found_pokemon.clear()
+                try:
+                    if call.data in ['keepgoing', 'skip']:
+                        bot.delete_message(call.message.chat.id, call.message.message_id)
+                       # удаляет сообщение в котором было нажато "keepgoing, skip"
+                    if call.data in ['skip']:
+                        bot.delete_message(call.message.chat.id, call.message.message_id-1)
+                except Exception as e:
+                    print(f"Ошибка при удалении сообщения: {e}")
+
+                if random.choice([True, False]):
+                    self.states[chat_id] = 'choose_catch_or_skip'
+                    self.show_catch_or_skip_buttons(chat_id, pokebol_count)
+                else:
+                    self.states[chat_id] = 'choose_find_or_skip'
+                    self.back_to_start(chat_id, call.message.message_id)
+
+            # Обработка нажатия кнопок "Catch", "Retry"
+            elif call.data in ['catch', 'retry']:
+                if call.data == 'retry':
+                    bot.delete_message(call.message.chat.id, call.message.message_id)
+
+                if random.choice([True, False]):
+                    self.states[chat_id] = 'choose_captured_or_retry'
+                    self.show_captured_or_retry_buttons(chat_id, call.message.message_id)
+
+                else:
+                    self.states[chat_id] = 'choose_find_or_skip'
+                    self.show_captured_or_not_buttons(chat_id, call.message.message_id)
+
+
+        else:
+            bot.send_message(chat_id, "У вас нет pokebol! Найдите или купите их, чтобы продолжить ловлю покемонов.")
+            #на будущее: нужно придумать какое то продолжение для пользователя после этого сообщения
+
+
+
+
+    def show_go_buttons(self, chat_id):
+        # Отправка кнопки "Go" для начала поиска покемона
+        markup = types.InlineKeyboardMarkup()
+
+        button_go = types.InlineKeyboardButton('Go', callback_data='go')
+        markup.add(button_go)
+
+        bot.send_message(chat_id, "Press 'Go' to start searching for a Pokemon:", reply_markup=markup)
+
+    def back_to_start(self, chat_id, message_id):
+        # Возвращение к начальному состоянию после неудачной попытки
+        markup = types.InlineKeyboardMarkup()
+        button_back = types.InlineKeyboardButton('Keep going', callback_data='keepgoing')
+        markup.add(button_back)
+        bot.send_message(chat_id, 'You did not find anything', reply_markup=markup)
+        #bot.delete_message(chat_id, message_id)
+
+
+    def show_pokedex(self, chat_id):
+        self.generator = functions.show_pokedex(chat_id)
+        markup = types.InlineKeyboardMarkup()
+        next_list = types.InlineKeyboardButton('Next', callback_data='next')
+        markup.add(next_list)
+        bot.send_message(chat_id, next(self.generator), reply_markup=markup)
+
+    def my_pokemons(self, chat_id):
+        pokemons = functions.my_pokemons(chat_id)
+        # Проверка на пустую строку 'pokedex' перед отправкой
+        if pokemons.strip() == '':
+            bot.send_message(chat_id, "No Pokemons have been captured yet.")
+        else:
+            bot.send_message(chat_id, pokemons)
+
+
+
+    def show_catch_or_skip_buttons(self, chat_id, pokebol_count ):
+        # Отображение кнопок "Try to Catch" и "Skip" после успешной попытки
+        markup = types.InlineKeyboardMarkup()
+        button_catch = types.InlineKeyboardButton('Try to Catch', callback_data='catch')
+        button_skip = types.InlineKeyboardButton('Skip', callback_data='skip')
+        markup.add(button_catch, button_skip)
+
+
+        # Отображение случайного покемона с весами
+        chosen_pokemon, gen = functions.pokemon_catch() #функция с вероятностями выпадения покемонов в файле functions.py
+        pokemon_image = f'images/{chosen_pokemon.capitalize()}.webp'
+        with open(pokemon_image, 'rb') as pokemon_photo:
+            found_pokemon.append(chosen_pokemon)
+            sent_message = bot.send_document(chat_id, pokemon_photo)
+            gen_info = functions.generations[chosen_pokemon]
+            if gen_info != '': gen_info = f' ({gen_info})'
+            bot.send_message(chat_id, f"You found a {chosen_pokemon}{gen_info}! It has '{gen}' rarity. What would you like to do?\nYou have {pokebol_count} pokebols",  reply_markup=markup)
+            self.states[chat_id] = {'message_id': sent_message.message_id, 'state': 'choose_catch_or_skip'}
+
+
+
+    def show_captured_or_retry_buttons(self, chat_id, message_id):
+        # Отображение кнопки "Keep going" после успешного захвата
+        markup = types.InlineKeyboardMarkup()
+        button_go = types.InlineKeyboardButton('Keep going', callback_data='go')
+        markup.add(button_go)
+        functions.capture_pokemon(chat_id, f"{found_pokemon[0]}")
+        bot.send_message(chat_id, f"You captured a {found_pokemon[0]}!", reply_markup=markup)
+
+        #bot.delete_message(chat_id, message_id)
+
+    def show_captured_or_not_buttons(self, chat_id, message_id):
+        # Отображение кнопки "Try again" после неудачной попытки захвата
+        markup = types.InlineKeyboardMarkup()
+        button_try_again = types.InlineKeyboardButton('Try again', callback_data='retry')
+        markup.add(button_try_again)
+        functions.capture_failed(chat_id)
+        bot.send_message(chat_id, 'Bad luck', reply_markup=markup)
+        #bot.delete_message(chat_id, message_id)
+
+    def get_pokebols(self, user_id):
+        with DatabaseConnection('pokedex.sql') as cur:
+            can_get_pokebols = functions.check_pokebols_elegibility(user_id) #возвращает True or False
+            text = functions.time_until_next_midnight()
+            if can_get_pokebols:
+                functions.add_pokebols(user_id, 50, cur)
+                bot.send_message(user_id, f'Вы получили 50 бесплатных покеболов. До следующего бесплатного получения осталось {text}')
+            else:
+                bot.send_message(user_id, f'К сожалению вы еще не можете получить бесплатные покеболы. Дождитесь следующего дня. Осталось ждать: {text}')
+        
+    def run(self):
+        # Запуск бота в режиме бесконечного опроса
+        bot.infinity_polling()
+
+# Если файл запущен напрямую (а не импортирован как модуль)
+if __name__ == "__main__":
+    # Создание экземпляра класса PokemonBot
+    pokemon_bot = PokemonBot()
+
+    # Обработчики сообщений и колбеков
+    @bot.message_handler(commands=['start'])
+    def start_wrapper(message):
+        pokemon_bot.start(message)
+
+    @bot.message_handler(commands=['pokedex'])
+    def deploy_pokedex(message):
+        chat_id = message.chat.id
+        pokemon_bot.show_pokedex(chat_id)
+
+    @bot.message_handler(commands=['go'])
+    def show_go_message(message):
+        chat_id = message.chat.id
+        pokemon_bot.show_go_buttons(chat_id)
+
+    @bot.message_handler(commands=['help'])
+    def help_command(message):
+        bot.send_message(message.chat.id, helpinfo, parse_mode='html')
+
+    @bot.message_handler(commands=['get_pokebols'])
+    def get_pokebols(message):
+        pokemon_bot.get_pokebols(message.chat.id)
+
+    @bot.message_handler(commands=['my_pokemons'])
+    def get_pokebols(message):
+        pokemon_bot.my_pokemons(message.chat.id)
+    
+    @bot.message_handler(commands=['rarity'])
+    def rarity_command(message):
+        bot.send_message(message.chat.id, rarity, parse_mode='html')
+
+    @bot.callback_query_handler(func=lambda call: call.data == "next")
+    def scroll_to_next(call):
+        markup = types.InlineKeyboardMarkup()
+        next_list = types.InlineKeyboardButton('Next', callback_data='next')
+        markup.add(next_list)
+        try:
+            bot.edit_message_text(next(pokemon_bot.generator), call.message.chat.id, call.message.message_id, reply_markup=markup)
+        except TypeError:
+            bot.edit_message_text('This Pokedex is not valid anymore, press /pokedex to get up-to-date version', call.message.chat.id, call.message.message_id)
+
+    @bot.callback_query_handler(func=lambda call: call.data in ['go', 'keepgoing', 'skip', 'retry', 'catch'])
+    def handle_go_callback_wrapper(call):
+        markup = types.InlineKeyboardMarkup()
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=markup)
+        pokemon_bot.handle_go_callback(call)
+
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_handler_wrapper(call):
+        markup = types.InlineKeyboardMarkup()
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=markup)
+        pokemon_bot.callback_handler(call)
+
+    # Запуск бота
+    pokemon_bot.run()
